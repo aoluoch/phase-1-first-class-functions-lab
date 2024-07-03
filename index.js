59345cc41ecb6fa6375a1118f012133fc47378bf
Amos Oluoch <aoluoch271@gmail.com>
2024-07-03 07:03:47 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){ 
+    return drivers.slice(0, 2);
+ }; 
+    
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])) 
+
+const returnLastTwoDrivers = function(drivers){ 
+    return drivers.slice(-2); 
+}; 
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
